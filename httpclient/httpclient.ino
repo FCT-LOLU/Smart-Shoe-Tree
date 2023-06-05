b37d6d11c2421f74f5597e918ffd84bfdef3050e
Lucie <Lucie BOUCHER>
2023-06-05 11:14:58 +0100

    read firebase

--- httpclient/httpclient.ino
+++ httpclient/httpclient.ino
@@ -1,10 +1,3 @@
-#include <sd_diskio.h>
-#include <sd_defines.h>
-#include <SD.h>
-
-//#include <FirebaseJson.h>
-
-
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <addons/TokenHelper.h>
@@ -17,8 +10,9 @@
 #define USER_EMAIL "lucie.boucher@telecomnancy.eu"
 #define USER_PASSWORD "test12"
 
-const char* ssid = "ulu";
-const char* password =  "lulumimi";
+
+const char* ssid = "ZON-AFC0";
+const char* password =  "f933af58161c";
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -42,6 +36,8 @@ const int MOTION_SENSOR_PIN = 32;
 int stateMotion=LOW;
 
 DHT dht(DHT_SENSOR_PIN, DHT_TYPE);
+
+String mask = "";
 
 // The Firestore payload upload callback function
 void fcsUploadCallback(CFS_UploadStatusInfo info)
@@ -71,13 +67,21 @@ void fcsUploadCallback(CFS_UploadStatusInfo info)
 void setup() {
 
   Serial.begin(115200);
+
   pinMode(2,OUTPUT);
+
+  //Initialize the sensor
   pinMode(MOTION_SENSOR_PIN,INPUT);
+
+  //Initialize the LED
   pinMode(BLUE_PIN,OUTPUT);
   pinMode(GREEN_PIN,OUTPUT);
   pinMode(RED_PIN,OUTPUT);
+
   setTime(0, 0, 0, 6, 5, 2023); 
+  //Initialize the DHT sensor
   dht.begin();
+
   
   WiFi.begin(ssid, password);
 
@@ -121,90 +125,62 @@ void loop() {
   char timestamp[25];
   sprintf(timestamp, "%04d-%02d-%02dT%02d:%02d:%02dZ",year(now), month(now), day(now), hour(now), minute(now), second(now));
   
- if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
   digitalWrite(2, HIGH);
- }else{
+  }else{
     Serial.println("Error in WiFi connection");   
- }
+  }
   delay(1000);  //Send a request every 1 second
   digitalWrite(2, LOW);
   delay(1000);
-  
-  String documentPath= "user/v16XDXhy5OaMuf0JTtIMQdh6cpH2/Humidity Rate Threshold";
-  String mask ="";
-  
-            
-  //retrieve Temperature threshold from Firebase
-  
-  /*if(humidity>=humidityRateMax){
-    digitalWrite(BLUE_PIN,HIGH);
-    digitalWrite(RED_PIN,LOW);
-  }else{
-    digitalWrite(BLUE_PIN,LOW);
-    digitalWrite(RED_PIN,HIGH);
-  } */
+  if (Firebase.ready() && (millis() - dataMillis > 60000 || dataMillis == 0)){
+    dataMillis = millis();
 
-  if (Firebase.ready() && (millis() - dataMillis > 60000 || dataMillis == 0))
-      {
-          dataMillis = millis();
-        
-        
-           // For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create.ino
-          FirebaseJson content;
+    FirebaseJson content;
 
-          // Note: If new document created under non-existent ancestor documents, that document will not appear in queries and snapshot 
-          // https://cloud.google.com/firestore/docs/using-console#non-existent_ancestor_documents.
+    String documentPath = "shoeData/time"+ String(count);
 
-          // We will create the document in the parent path "a0/b?
-          // a0 is the collection id, b? is the document id in collection a0.
+    //String
+    content.set("fields/IDofShoe/stringValue","hZBzDnPYJXmkqQo6oIeu");
 
-          String documentPath = "shoeData/time" + String(count);
+    // integer
+    content.set("fields/light/integerValue", analogValue);
+    content.set("fields/motion/integerValue", stateMotion);
+    
+    //double
+    content.set("fields/humidity/doubleValue", humidity);
+    content.set("fields/temperature/doubleValue", temp);
 
-          // If the document path contains space e.g. "a b c/d e f"
-          // It should encode the space as %20 then the path will be "a%20b%20c/d%20e%20f"
-
-          //String
-          content.set("fields/IDofShoe/stringValue","hZBzDnPYJXmkqQo6oIeu");
-     
-          // integer
-          content.set("fields/light/integerValue", analogValue);
-          content.set("fields/motion/integerValue", stateMotion);
-          
-          //double
-          content.set("fields/humidity/doubleValue", humidity);
-          content.set("fields/temperature/doubleValue", temp);
-
-          // timestamp
-          content.set("fields/date/timestampValue", timestamp); // RFC3339 UTC "Zulu" format
+    // timestamp
+    content.set("fields/date/timestampValue", timestamp); // RFC3339 UTC "Zulu" format
 
 
-          count++;
+    count++;
 
-          Serial.print("Create a document... ");
+    Serial.print("Create a document... ");
 
-          if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, documentPath.c_str(), content.raw()))
-              Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-          else
-              Serial.println(fbdo.errorReason());
+    if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, documentPath.c_str(), content.raw())){
+        Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
 
-          //digitalWrite(BLUE_PIN,HIGH);
-          //retrieve Temperature threshold from Firebase
-          if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), mask.c_str())){
-            Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-            humidityFirebase= fbdo.payload().c_str();
-            humidityRateMax = humidityFirebase.toDouble();
-            digitalWrite(GREEN_PIN,HIGH);
-          }else{
-            Serial.println(fbdo.errorReason());
-            digitalWrite(RED_PIN,HIGH);
-          }
-          
-          if(humidityRateMax==0.0){
-            digitalWrite(BLUE_PIN,HIGH);
-          }else{
-            digitalWrite(RED_PIN,HIGH);
-          }
-          
-      }
+        // get the Humidity Rate Threshold of fbdo.payload().c_str()
+        double humidityRateThreshold = fbdo.payload().substring(fbdo.payload().indexOf("Humidity Rate Threshold") + 23, fbdo.payload().indexOf("Humidity Rate Threshold") + 27).toDouble();
+        Serial.print("Humidity Rate Threshold: ");
+        Serial.println(humidityRateThreshold); // 40.8
+    }else
+        Serial.println(fbdo.errorReason());
+
+    documentPath = "user/lSYK0UAxhqcYKK6z4zXHfuMK58w1";
+    mask = "";
+    
+    if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), mask.c_str())){
+      Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+      
+    }else{
+      Serial.println(fbdo.errorReason());
+      
+    }
+      
+  }
 
 }
+
